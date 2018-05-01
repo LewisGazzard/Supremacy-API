4eab4b015c115d208bb8c7986a06efa2df6bd609
Lewis Gazzard <lewis.gazzard@boomsatsuma.agency>
2018-05-01 15:33:56 +0100

    V2.3a
    Version 2.2a
    
    Fixes: Logo Change, Removed VAT.

---
+++ lib/config.ini.php
@@ -0,0 +1,31 @@
+<?php
+	/**
+	* Configuration
+
+	* @package Wojo Framework
+	* @author wojoscripts.com
+	* @copyright 2018
+	* @version Id: config.ini.php, v1.00 2018-02-06 04:51:03 gewa Exp $
+	*/
+
+	 if (!defined("_WOJO"))
+     die('Direct access to this location is not allowed.');
+
+	/**
+	* Database Constants - these constants refer to
+	* the database configuration settings.
+	*/
+	 define('DB_SERVER', '10.16.16.1');
+	 define('DB_USER', 'users-apa-u-163874');
+	 define('DB_PASS', 'Justmedia123!');
+	 define('DB_DATABASE', 'users-apa-u-163874');
+	 define('DB_DRIVER', 'mysql');
+
+	 define('INSTALL_KEY', 'ubZB1NPdNs7l0A8Q');
+
+	/**
+	* Show Debugger Console.
+	* Display errors in console view. Not recomended for live site. true/false
+	*/
+	 define('DEBUG', false);
+?>
---
+++ view/front/activation.tpl.php
@@ -0,0 +1,12 @@
+<?php
+  /**
+   * Activation
+   *
+   * @package Wojo Framework
+   * @author wojoscripts.com
+   * @copyright 2016
+   * @version $Id: activation.tpl.php, v1.00 2016-01-08 10:12:05 gewa Exp $
+   */
+  if (!defined("_WOJO"))
+      die('Direct access to this location is not allowed.');
+?>
